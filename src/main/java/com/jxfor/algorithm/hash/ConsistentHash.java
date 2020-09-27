828eae4cbc2352a5df56951714b29923f80b0cde
何健 <hej@doordu.com>
2020-09-27 12:00:09 +0800

    添加一致性hash算法

---
+++ src/main/java/com/jxfor/algorithm/hash/ConsistentHash.java
@@ -0,0 +1,83 @@
+package com.jxfor.algorithm.hash;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * 一致性 hash
+ *
+ * @author jxfor
+ * @date 2020/9/27 10:20
+ */
+public class ConsistentHash {
+
+    private static SortedMap<Integer,String> sortedMap = new TreeMap<>();
+
+    private static int addServer( String server,int virtualNodeCount) {
+        if( virtualNodeCount < 0 ){
+            return 0;
+        }
+
+        int successCount = 0 ,hash = 0;
+        String virtualServer;
+        for( int i = 0 ; i < virtualNodeCount + 10 ;i++){
+            virtualServer = server+"#"+i;
+            hash = getHash(virtualServer);
+            if( sortedMap.containsKey(hash)){
+                continue;
+            }
+            sortedMap.put(hash,server+"#"+i);
+            successCount++;
+            if( successCount > virtualNodeCount){
+                break;
+            }
+        }
+
+        return successCount;
+    }
+
+    private static String getServer( int hash ){
+        SortedMap<Integer, String> resultMap = sortedMap.tailMap(hash);
+
+        hash = resultMap.isEmpty() ? sortedMap.firstKey() : resultMap.firstKey();
+
+        String server = sortedMap.get(hash);
+        //去除 #
+        int pos = server.lastIndexOf("#");
+        return pos == -1 ? server : server.substring(0,pos);
+    }
+
+    private static int getHash( String str ){
+        final int p = 16777619;
+        int hash = (int) 2166136261L;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash ^ str.charAt(i)) * p;
+        }
+        hash += hash << 13;
+        hash ^= hash >> 7;
+        hash += hash << 3;
+        hash ^= hash >> 17;
+        hash += hash << 5;
+
+        // 如果算出来的值为负数则取其绝对值
+        if (hash < 0) {
+            hash = Math.abs(hash);
+        }
+        return hash;
+    }
+
+    public static void main(String[] args) {
+        addServer("127.0.0.1:8080",2);
+        addServer("127.0.0.1:8081",2);
+        addServer("127.0.0.1:8082",2);
+
+        //查询节点
+        String[] arr =  new String[]{"he#2","he#1","x1"};
+        for( String str : arr ){
+            int hash = getHash(str);
+            System.out.println( getServer(hash));
+        }
+
+    }
+
+}
